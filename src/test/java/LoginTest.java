ce746737e269466cccdaf0b1dbf6782290b912af
MashaChornaya <105201355+MashaChornaya@users.noreply.github.com>
2022-08-27 20:47:37 +0300

    тесты selenide ошибка при запуске тестов  java.lang.NoSuchMethodError: 'org.openqa.selenium.chromium.ChromiumOptions org.openqa.selenium.chrome.ChromeOptions.setHeadless(boolean)'

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,54 +1,64 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class LoginTest {
 
+private final static String EMAIL="mashabigdreams@gmail.com";
+private final static String PASSWORD="sawdas5620";
+private final static String PROJECT="Best project";
+private final static String TESTCASE="Payment";
+private final static String DEFECT="Bad project";
+private final static String SEARCH_TEST_CASE= "//div[@data-handler-id]/descendant::div[text()='%s']";
+private final static String SEARCH_DEFECT= "//a[@class='defect-title'and text()='%s']";
+    @BeforeMethod
+    public void navigate() {
+        open("https://app.qase.io/login");
+    }
     @Test
     public void login(){
-        open("https://app.qase.io/login");
-        $("#inputEmail").setValue("mashabigdreams@gmail.com");
-        $("#inputPassword").setValue("sawdas5620");
+        $("#inputEmail").setValue(EMAIL);
+        $("#inputPassword").setValue(PASSWORD);
         $(".custom-control-indicator").click();
         $("#btnLogin").click();
         $(byXpath("//h1")).shouldHave(text("Projects"));
+        Assert.assertTrue($(By.xpath("//h1[text()='Projects']")).isDisplayed());
     }
     @Test
     public void createTestCase(){
-        open("https://app.qase.io/login");
         $("#inputEmail").setValue("mashabigdreams@gmail.com");
         $("#inputPassword").setValue("sawdas5620");
         $(".custom-control-indicator").click();
         $("#btnLogin").click();
         $(byXpath("//h1")).shouldHave(text("Projects"));
         $("#createButton").click();
-        $(byXpath("//input[@name='title']")).setValue("Best project");
-        $(byXpath("//span[@class='vrNjrd']")).click();
-        $(byXpath("//div[@class='DJXdnf']")).click();
+        $(byXpath("//input[@name='title']")).setValue(PROJECT);
         $("#public-access-type").click();
         $(byXpath("//button[@type='submit']")).click();
         $("#create-case-button").click();
-        $("#title").setValue("Payment");
-        $(byXpath("//input[@placeholder='Type to search']")).click();
-        $(byXpath("//div[@class='DJXdnf' and text()='High']")).click();
-        $(byXpath("//div[@class='RZYgph' and text()='Actual']")).click();
-        $(byXpath("//div[@class='DJXdnf' and text()='Smoke']")).click();
+        $("#title").sendKeys(TESTCASE);
+        $("#suite").sendKeys("Test cases without suite");
         $("#save-case").click();
-        $(byXpath("//span[@class='OL6rtE' and text()='Test case was created successfully!']")).shouldHave(text("Test case was created successfully!"));
+        $("span.OL6rtE").shouldHave(text("Test case was created successfully!"));
+        Assert.assertEquals(($(By.xpath(String.format(SEARCH_TEST_CASE, TESTCASE))).getText()),TESTCASE);
     }
     @Test
     public void createDefects(){
-        open("https://app.qase.io/login");
         $("#inputEmail").setValue("mashabigdreams@gmail.com");
         $("#inputPassword").setValue("sawdas5620");
         $(".custom-control-indicator").click();
         $("#btnLogin").click();
         $(byXpath("//h1")).shouldHave(text("Projects"));
         $("#createButton").click();
-        $(byXpath("//input[@name='title']")).setValue("Bad project");
+        $(byXpath("//input[@name='title']")).setValue(PROJECT);
         $("#public-access-type").click();
         $(byXpath("//button[@type='submit']")).click();
         $(byXpath("//span[@class='nbWgel' and text()='Defects']")).click();
@@ -62,6 +72,11 @@ public class LoginTest {
                 "\n" +
                 "Actual result: Nothing found");
         $(byXpath("//button[@type='submit']")).click();
-        $(byXpath("//span[@class='OL6rtE' and text()='Defect was created successfully!']")).shouldHave(text("Defect was created successfully!"));
+        $(byXpath("//span[@class='OL6rtE']")).shouldHave(text("Defect was created successfully!"));
+        Assert.assertEquals(($(byXpath(String.format(SEARCH_DEFECT,DEFECT))).getText()),DEFECT);
+    }
+    @AfterMethod
+    public void close() {
+        getWebDriver().quit();
     }
 }
